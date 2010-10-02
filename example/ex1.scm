9ea9bc45af41883ba9b50143318bf532e9bc6c60
Takuya Mannami <mtakuya@users.sourceforge.jp>
2010-10-03 00:13:37 +0900

    0.0.1

--- example/ex1.scm
+++ example/ex1.scm
@@ -1,5 +1,5 @@
 #!/usr/bin/env gosh
 
 (use ja-input)
-(print (caar (string->ja-input-list "なんかおもしろそうだったのでらいぶらりをかいてみた")))
-;(print (string->ja-input-list "もじれつをわたすとりすとがかえってきます"))
+;(print (caar (string->ja-input-list "なんかおもしろそうだったのでらいぶらりをかいてみた")))
+(print (string->ja-input-list "もじれつをわたすとりすとがかえってきます"))
